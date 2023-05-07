c42332cf6ce17626eeeb17c6d9ec14eea9d002aa
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-05-07 15:58:59 +1000

    Setup routers and controllers for the app.

---
+++ controllers/tasks.js
@@ -0,0 +1,24 @@
+const getAllTasks = (req, res) => {
+  res.send("get all tasks");
+};
+
+const createTask = (req, res) => {
+  res.send("Create task");
+};
+const getTask = (req, res) => {
+  res.send("Get task");
+};
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+};
