564368b3e4dd9fae7ffe85ebcc16e7cee9329d28
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-05-08 11:50:26 +1000

    Create .env file.

--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,12 @@
-const connection =
-  "mongodb+srv://yasmin:<password>@nodeexpressprojects.onxphhn.mongodb.net/?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
